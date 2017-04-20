3afe3566800913e1cd47221a0d8dec1812fc2253
Alice Blanc <alice.blanc@laposte.net>
2017-04-20 19:21:25 +0200

    feuille

--- src/Feuille.cpp
+++ src/Feuille.cpp
@@ -7,3 +7,46 @@
 //
 
 #include "Feuille.hpp"
+
+
+Feuille::Feuille(){
+    
+}
+
+Feuille(int g1, int g2)
+{
+    this->gainJ1 = g1;
+    this->gainJ2 = g2 ;
+}
+
+Feuille::getGainJ1(){
+    return this.gainJ1
+}
+
+Feuille::getGainJ2(){
+    return this.gainJ2
+}
+
+Feuille::comparerGainJ1(Feuille feuille){
+    if (this.getGainJ1() < feuille.getGainJ1()) {
+        return ("inferieur")
+    } else {
+        if (this.getGainJ1() > feuille?getGainJ1()) {
+            return ("superieur")
+        } else {
+            return ("egal")
+        }
+    }
+}
+
+Feuille::comparerGainJ2(Feuille feuille){
+    if (this.getGainJ2() < feuille.getGainJ2()) {
+        return ("inferieur")
+    } else {
+        if (this.getGainJ2() > feuille?getGainJ2()) {
+            return ("superieur")
+        } else {
+            return ("egal")
+        }
+    }
+}
--- src/Feuille.hpp
+++ src/Feuille.hpp
@@ -13,9 +13,20 @@
 #include "Arbre.hpp"
 
 class Feuille : Arbre {
-    private int gains[] ;
     
+private:
+    
+    int gainj1 ;
+    int gainJ2 ;
+
 public:
+    
+    Feuille() ;
+    Feuille(int,int);
+    getGainJ1();
+    getGainJ2();
+    comparerGainJ1(Feuille feuille) ;
+    comparerGainJ2(Feuille feuille) ;
     
 };
 #endif /* Feuille_hpp */
